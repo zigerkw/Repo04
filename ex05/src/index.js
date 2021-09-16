fe463f5610a62e4ee02343b26a37b5a2b09e994a
zigerkw <zejdhadzic@hotmail.com>
2021-09-16 19:03:43 +0300

    peta vjezba

---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+function main (numOne, numTwo) {
+    // only change code below this line
+    var product = numOne * numTwo;
+    // only change code above this line
+    return product;
+    
+    }
+    
+    console.log(main(100.25, 43.20));
+    module.exports = main;
