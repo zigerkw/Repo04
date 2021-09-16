a622f19321666e376abd7847901493cf6ee9416f
zigerkw <zejdhadzic@hotmail.com>
2021-09-16 18:56:17 +0300

    cetvrta vjezba

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function main () {
+    // only change code below this line
+    var myDecimal = 8.2;
+    var myDecimalE = 0.00254;
+    // only change code above this line
+    return {
+        myDecimal,
+        myDecimalE
+    };
+    
+    }
+    
+    console.log(main());
+    module.exports = main;
