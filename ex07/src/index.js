712b5ce2f0751a8a404d6432cf07deac06604496
zigerkw <zejdhadzic@hotmail.com>
2021-09-16 19:20:59 +0300

    sedma vjezba

---
+++ ex07/src/index.js
@@ -0,0 +1,10 @@
+function main (numOne, numTwo) {
+    // only change code below this line
+    var remainder = numOne % numTwo;
+    // only change code above this line
+    return remainder;
+    
+    }
+    
+    console.log(main(11, 5));
+    module.exports = main;
