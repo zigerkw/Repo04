1f06c1969734c7ae438da040252978d24f8d9b8b
zigerkw <zejdhadzic@hotmail.com>
2021-09-16 19:52:52 +0300

    deseta vjezba

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main () {
+    // only change code below this line
+    var myFirstName = "Zejd";
+    var myLastName = "Hadzic";
+    var myHomeTown = "Tomislavgrad";
+    
+    // only change code above this line
+    return {
+    myFirstName,
+    myLastName,
+    myHomeTown    
+    };
+    
+    }
+    
+    console.log(main());
+    module.exports = main;
