54e7669a81e8d6f30702d0420dd3894258b4fcc9
zigerkw <zejdhadzic@hotmail.com>
2021-09-16 19:12:37 +0300

    Sesta vjezba

---
+++ ex06/src/index.js
@@ -0,0 +1,10 @@
+function main (numOne, numTwo) {
+    // only change code below this line
+    var myQuotient = numOne / numTwo;
+    // only change code above this line
+    return myQuotient;
+    
+    }
+    
+    console.log(main(102.0, 25.5));
+    module.exports = main;
