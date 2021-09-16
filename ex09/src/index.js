29f4041c94a9f8a6d96b52761e65b0708ccb68aa
zigerkw <zejdhadzic@hotmail.com>
2021-09-16 19:45:49 +0300

    deveta vjezba

---
+++ ex09/src/index.js
@@ -0,0 +1,19 @@
+function main () {
+    // only change code below this line
+        var firstNum = 6;
+        firstNum *= 9;
+        var secondNum = 25;
+        secondNum /= 5;
+        var thirdNum = 5.2;
+        thirdNum *=10;
+    // only change code above this line
+    return {
+        firstNum,
+        secondNum,
+        thirdNum
+    };
+    
+    }
+    
+    console.log(main());
+    module.exports = main;
